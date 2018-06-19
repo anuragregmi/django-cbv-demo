22c2ff0c2bf0e0faeb33969103f3910f21788ba5
Anurag Regmi <anuragregmi@protonmail.com>
2018-06-19 13:42:05 +0545

    Implement base view with required methods

--- main/views.py
+++ main/views.py
@@ -1,10 +1,63 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
+
 
 class BaseView:
     http_method_names = ['get', 'post', 'put', 'patch', 'delete', 'head', 'options', 'trace']
 
-    def as_view(*args, **kwargs):
-        return lambda x: HttpResponse("HelloWorld")
+    def __init__(self, **kwargs):
+        """
+        Constructor. Called in the URLconf; can contain helpful extra
+        keyword arguments, and other things.
+        """
+        # Go through keyword arguments, and either save their values to our
+        # instance, or raise an error.
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    @classmethod
+    def as_view(cls, **initkwargs):
+        """Main entry for request response process"""
+
+        def view(request, *args, **kwargs):
+            # function that will be returned by View class.
+            # Called when request is received.
+            self = cls(**initkwargs)
+
+            if hasattr(self, 'get') and not hasattr(self, 'head'):
+                self.head = self.get
+
+            self.request = request
+            self.args = args
+            self.kwargs = kwargs
+
+            return self.dispatch(request, *args, **kwargs)
+
+        view.view_class = cls
+        view.view_initkwargs = initkwargs
+
+        return view
+
+    def dispatch(self, request, *args, **kwargs):
+        # Try to dispatch to the right method; if a method doesn't exist,
+        # defer to the error handler. Also defer to the error handler if the
+        # request method isn't on the approved list.
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
+        else:
+            handler = self.http_method_not_allowed
+        return handler(request, *args, **kwargs)
+
+    def http_method_not_allowed(self, request, *args, **kwargs):
+        # Try to dispatch to the right method; if a method doesn't exist,
+        # defer to the error handler. Also defer to the error handler if the
+        # request method isn't on the approved list.
+        return HttpResponseNotAllowed(self._allowed_methods(),
+                                      content="`{}` method is not allowed".format(request.method))
+
+    def _allowed_methods(self):
+        return [m.upper() for m in self.http_method_names if hasattr(self, m)]
+
 
 class TestView(BaseView):
-    pass
+    def get(self, request):
+        return HttpResponse("Hello World")
